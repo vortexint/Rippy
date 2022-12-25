2ce87bcb72b2fdca06a9b276afb0d699f379ed6c
Igor Alexey <igor-alexey@hotmail.com>
2022-12-25 01:32:05 -0300

    change display message

--- main.cpp
+++ main.cpp
@@ -184,7 +184,11 @@ int main(int argc, char* argv[])
             break;
         }
         // single line statistics (time elapsed & pages crawled)
-        std::cout << "\r" << std::chrono::duration_cast<std::chrono::seconds>(std::chrono::high_resolution_clock::now() - startTime).count() << "s elapsed, " << config.visitedPages.size() << " pages crawled, " << linkBuffer.visitedSize() << " visited links, " << linkBuffer.queueSize() << " links in queue";
+        std::cout << "\r" << std::chrono::duration_cast<std::chrono::seconds>(std::chrono::high_resolution_clock::now() - startTime).count() <<
+        "s elapsed, " <<
+        config.visitedPages.size() << " pages crawled, " <<
+        linkBuffer.visitedSize() << " visited links, " <<
+        linkBuffer.queueSize() << " links in queue";
     }
 
 
